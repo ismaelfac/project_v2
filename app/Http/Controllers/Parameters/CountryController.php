1364be8212eab5683c0b4cadf30cfa837fc4c11d
Ismael E. Lastre Alvarez <ismaelfac@gmail.com>
2019-11-08 21:14:22 -0500

    'configuration_cors_ok'

--- app/Http/Controllers/Parameters/CountryController.php
+++ app/Http/Controllers/Parameters/CountryController.php
@@ -20,9 +20,7 @@ class CountryController extends Controller
      */
     public function create()
     {
-        $roles_unique = Role::where('special', 'all-access')->orWhere('special', 'no-access')->get();
-        $roles_personalized = Role::where('special', null)->paginate(5);
-        return view('admin2.modules.users.create', compact('roles_unique', 'roles_personalized'));
+        
     }
 
     /**
@@ -33,8 +31,7 @@ class CountryController extends Controller
      */
     public function store(Request $request)
     {
-        $permission = Permission::create($request::all());
-        return redirect()->route('permissions.edit', $permission->id)->with('info', 'Usuario Guardado con Exito');
+ 
     }
 
     /**
@@ -43,7 +40,7 @@ class CountryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(Permission $permission)
+    public function edit(Country $country)
     {
         return view('admin2.modules.permissions.edit', compact('permission'));
     }
@@ -57,9 +54,7 @@ class CountryController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        $user->update($request::all()); //update user
-        $user->roles()->sync($request->get('roles')); //update roles
-        return redirect()->route('users.edit', $user->id)->with('info', 'Usuario Actualizado con Exito');
+  
     }
 
     /**
@@ -70,12 +65,6 @@ class CountryController extends Controller
      */
     public function destroy(User $user)
     {
-        if ($user->is_active) {
-            $user->update(['is_active' => false]);
-            return back()->with('info', 'Usuario Desactivado Correctamente');
-        } else {
-            $user->update(['is_active' => true]);
-            return back()->with('info', 'Usuario Activado Correctamente');
-        }
+        
     }
 }
