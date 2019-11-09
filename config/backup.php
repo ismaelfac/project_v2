afcaaec6d9a1e63feb138ffd6f7e449e7e4418f1
Ismael E. Lastre Alvarez <ismaelfac@gmail.com>
2019-11-08 20:36:25 -0500

    'Backup_configuration'

--- config/backup.php
+++ config/backup.php
@@ -131,7 +131,7 @@ return [
         'notifiable' => \Spatie\Backup\Notifications\Notifiable::class,
 
         'mail' => [
-            'to' => 'your@example.com',
+            'to' => env('MAIL_AUTHOR','ismaelfac@gmail.com'),
 
             'from' => [
                 'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
