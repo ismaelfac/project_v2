3bf641f134486c50e7902b414a323f4f28dc3355
ISMAEL E. LASTRE ALVAREZ <ismaelfac@gmail.com>
2019-11-15 23:28:16 -0500

    Repositories

---
+++ database/factories/ModelsPostFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ModelsPost;
+use Faker\Generator as Faker;
+
+$factory->define(ModelsPost::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
